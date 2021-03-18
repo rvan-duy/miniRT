63b2cbf5aa0b729236695d61867f5d4b30303f97
rvan-duy <rvanduyneveldt@gmail.com>
2021-03-18 01:29:14 +0100

    norm

--- math/mrt_math_magnitude_calc.c
+++ math/mrt_math_magnitude_calc.c
@@ -6,23 +6,23 @@
 /*   By: rvan-duy <rvan-duy@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2021/03/18 00:16:29 by rvan-duy      #+#    #+#                 */
-/*   Updated: 2021/03/18 01:27:08 by rvan-duy      ########   odam.nl         */
+/*   Updated: 2021/03/18 01:28:58 by rvan-duy      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/miniRT.h" // t_vector
 #include <math.h> // sqrt
 
-double  mrt_math_magnitude_calc(t_vector *vec)
+double	mrt_math_magnitude_calc(t_vector *vec)
 {
-    double x;
-    double y;
-    double z;
-    double magnitude;
+	double	x;
+	double	y;
+	double	z;
+	double	magnitude;
 
-    x = vec->head.x - vec->tail.x;
-    y = vec->head.y - vec->tail.y;
-    z = vec->head.z - vec->tail.z;
-    magnitude = sqrt((x * x) + (y * y) + (z * z));
-    return (magnitude);
+	x = vec->head.x - vec->tail.x;
+	y = vec->head.y - vec->tail.y;
+	z = vec->head.z - vec->tail.z;
+	magnitude = sqrt((x * x) + (y * y) + (z * z));
+	return (magnitude);
 }
